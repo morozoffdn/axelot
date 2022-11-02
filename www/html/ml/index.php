9af1af8c0a3d7cc2a21452386518ac6738a94876
Morozov Dmitry <morozoffdn@gmail.com>
2022-11-02 09:41:13 +0300

    axelot

---
+++ www/html/ml/index.php
@@ -0,0 +1,8 @@
+<link rel="shortcut icon" href="#" />
+<?php
+
+echo '<p>axelot.ml!!!</p>';
+
+phpinfo();
+
+?>
---
+++ www/html/tk/index.php
@@ -0,0 +1,8 @@
+<link rel="shortcut icon" href="#" />
+<?php
+
+echo '<p> axelot.tk </p>';
+
+phpinfo();
+
+?>
